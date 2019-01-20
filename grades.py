b5acadab47f62536fee095aa956e4b50986f3a8f
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2019-01-19 19:16:28 -0800

    Refactor with zip function to simplify for loop

--- grades.py
+++ grades.py
@@ -1,19 +1,10 @@
-names = list(input("Please enter names separated by commas: ").split(","))
-assignments = list(input("Please enter assignments separated by commas: ").split(","))
-grades = list(input("Please enter grades separated by commas: ").split(","))
+names = input("Enter names separated by commas: ").title().split(",")
+assignments = input("Enter assignment counts separated by commas: ").split(",")
+grades = input("Enter grades separated by commas: ").split(",")
 
 message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
 submit before you can graduate. You're current grade is {} and can increase \
 to {} if you submit all assignments before the due date.\n\n"
 
-def max_grade(gr,assig):
-    gr = int(gr)
-    assig = int(assig)
-    return str(gr + assig * 2)
-
-for i in range(0,len(names)):
-    print(message.format(names[i].title(),
-    assignments[i],
-    grades[i],
-    max_grade(grades[i],assignments[i])
-    ))
+for name, assignment, grade in zip(names, assignments, grades):
+    print(message.format(name, assignment, grade, int(grade) + int(assignment)*2))
