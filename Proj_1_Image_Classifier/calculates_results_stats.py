b7646f8f8aeca247dae32c0715d3b363e1b20b28
Adam Kahn <adamkahn@adamkahn-macbookpro.roam.corp.google.com>
2019-02-12 10:55:28 -0800

    Adjust key referenced in dictionary --> 2 and 3 became 3 and 4

--- Proj_1_Image_Classifier/calculates_results_stats.py
+++ Proj_1_Image_Classifier/calculates_results_stats.py
@@ -126,7 +126,7 @@ def calculates_results_stats(results_dic):
         else:
             # Classifier classifies image as NOT a Dog(& pet image isn't a dog)
             # counts number of correct NOT dog clasifications.
-            if (results_dic[key][2] == 1 and results_dic[key][3] == 0):
+            if (results_dic[key][3] == 1 and results_dic[key][4] == 0):
                 results_stats_dic['n_correct_notdogs'] += 1
 
 
